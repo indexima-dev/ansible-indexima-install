d8dc02a740b6f26f4b9a766370c5bf47e53c440f
NicoGGG <nicolas.guelfi@indexima.com>
2020-10-07 11:17:32 +0200

    Molecule EC2 (#7)
    * added a scenario on aws ec2 ; docker scenario is now lighter
    
    * change security group in create to make indexima work ; fix ubuntu image owner ; add empty files folder
    
    * trying to create ephemeral folder before starting script
    
    * creating instance_config.yml before running test
    
    * adding debug to molecule test ec2
    
    * trying without ubuntu ; trying with ec2 only on debian
    
    * puting multiple env back
    
    * customize ssh key name and instance name to avoid conflicts during parallel run
    
    * removing debug
    
    * add name to instance 1
    
    * moving test to us east
    
    * testing only amazon linux for now

---
+++ travis/script_molecule.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### Travis script in sh file for local testing
+### Needs to be executed in the root directory of the role
+
+## If MOLECULE_AMI_NAME is set, scenario to play is the one with the EC2
+## Otherwise, the local docker scenarion is played
+
+set -e
+
+mkdir -p $HOME/.cache/molecule/ansible-indexima-install/aws-ec2
+touch $HOME/.cache/molecule/ansible-indexima-install/aws-ec2/instance_config.yml
+
+if [ ! -z "${MOLECULE_AMI_NAME}" ]; then
+  molecule test -s aws-ec2
+else
+  molecule test -s default
+fi
