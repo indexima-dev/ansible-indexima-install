a8f6db52afe338242f76772c2cae9d50f0a47491
NicoGGG <coolranger@protonmail.com>
2020-11-03 16:55:01 +0100

    added tests for jmx variable

--- travis/script_local_test.sh
+++ travis/script_local_test.sh
@@ -13,7 +13,7 @@ sudo docker build --file=travis/Dockerfile.${distribution}-${version} --tag=${di
 container_id=$(mktemp)
 sudo docker run --detach --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --volume="${PWD}":/etc/ansible/roles/ansible-indexima-install:ro \
     ${distribution}-${version}:ansible > "${container_id}"
-  
+
 sudo docker exec "$(cat ${container_id})" env ANSIBLE_FORCE_COLOR=1 bash -c 'cd /etc/ansible/roles/ansible-indexima-install && molecule test -s local'
 
 ## Cleanup
