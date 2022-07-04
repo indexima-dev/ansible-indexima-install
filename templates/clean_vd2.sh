a660daade8204e3351989ddc50a2712986f589e2
Kevin <kevin.torres@indexima.com>
2022-07-04 11:19:51 +0200

    remove all the project

--- templates/clean_vd2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find {{ indexima_path }}/visualdoop2/lib -name '*\.jar' -type f -delete
-find {{ indexima_path }}/visualdoop2/driver -name '*\.jar' -type f -delete
-find {{ indexima_path }}/visualdoop2 -name 'visualdoop2*\.war' -type f -delete
